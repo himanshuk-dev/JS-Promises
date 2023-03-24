0e1df584fcbca6930192f825a77e764d8fee7f93
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-23 19:12:25 -0700

    displaying card on button click

--- card-app.js
+++ card-app.js
@@ -1,6 +1,6 @@
 let baseURL = "https://deckofcardsapi.com/api/deck";
 
-/***** Solution 1*****/
+/***** Solution 1 *****/
 axios.get(`${baseURL}/new/draw/`).then((res) => {
   let { suit, value } = res.data.cards[0];
   console.log(
@@ -22,4 +22,32 @@ axios.get(`${baseURL}/new/draw/`).then((res) => {
       `Card 2: ${secondCard.value} of ${secondCard.suit}`
     );
   });
+});
+
+/***** Solution 3 *****/
+let btn = document.getElementById("button");
+
+btn.addEventListener("click", function setup() {
+  let cardContainer = document.querySelector("#card-container");
+
+  axios
+    .get(`${baseURL}/new/shuffle/`)
+    .then((res) => {
+      return axios.get(`${baseURL}/${res.data.deck_id}/draw/`);
+    })
+    .then((res) => {
+      console.log("button", res);
+      let cardSrc = res.data.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      let img = document.createElement("img");
+      img.setAttribute("src", cardSrc);
+
+      img.setAttribute(
+        "style",
+        `transform: translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+      );
+      cardContainer.append(img);
+    });
 });
