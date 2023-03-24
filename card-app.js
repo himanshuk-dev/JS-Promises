179574a07661cb035eca8ef5b8426a1600b5c7b7
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-23 18:35:08 -0700

    adding functionality to draw two cards from same deck

--- card-app.js
+++ card-app.js
@@ -1,7 +1,25 @@
 let baseURL = "https://deckofcardsapi.com/api/deck";
 
-/***** Solution 1 *****/
+/***** Solution 1*****/
 axios.get(`${baseURL}/new/draw/`).then((res) => {
   let { suit, value } = res.data.cards[0];
-  console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+  console.log(
+    "Draw single card from a deck >>>>",
+    `${value.toLowerCase()} of ${suit.toLowerCase()}`
+  );
+});
+
+/***** Solution 2 *****/
+
+axios.get(`${baseURL}/new/draw/`).then((res) => {
+  let deckId = res.data.deck_id;
+  let firstCard = res.data.cards[0];
+
+  axios.get(`${baseURL}/${deckId}/draw/`).then((res) => {
+    let secondCard = res.data.cards[0];
+    console.log(
+      `Card 1: ${firstCard.value} of ${firstCard.suit}`,
+      `Card 2: ${secondCard.value} of ${secondCard.suit}`
+    );
+  });
 });
